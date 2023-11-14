fb5f779999d9d5f218ae86337cf0a0222168edb4
dardanqsot <quispesotodaniel@gmail.com>
2023-11-13 22:05:48 -0500

    Add manytoOne Patient in VitalSign entity

--- mediapp-backend/src/main/java/com/mitocode/model/VitalSign.java
+++ mediapp-backend/src/main/java/com/mitocode/model/VitalSign.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 
 @Data
 @NoArgsConstructor
@@ -19,11 +21,18 @@ public class VitalSign {
     @EqualsAndHashCode.Include
     private Integer idSign;
 
+    @ManyToOne
+    @JoinColumn(name = "id_patient", nullable = false, foreignKey = @ForeignKey(name = "FK_CONSULT_PATIENT"))
+    private Patient patient;
+
     @Column(nullable = false, length = 60, unique = true)
     private String name;
 
     @Column(nullable = false, length = 10,  unique = true)
     private String abbreviation;
+
+    @Column(nullable = false)
+    private LocalDateTime vitalSignDate;
 
     @Column(nullable = false)
     private boolean enabled;
